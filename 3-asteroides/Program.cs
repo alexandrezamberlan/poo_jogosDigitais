a648ae056af8052c65ec40706b5e676cf81588c8
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-04-08 13:33:05 -0300

    jogo dos asteroides

---
+++ 3-asteroides/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1_jogos
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Jogo dos Asteróides");
+            //definir quantos asteróides o sistema irá trabalhar
+            
+            //gerar e armazenar asteróides em lista 
+            //(posições, tamanhos e velocidades gerados aleatoriamente)
+
+            //gerar e armazenar barreiras em lista
+
+            //exibir os dados de cada asteróide (x,y,tamanho,velocidade)
+
+            //exibir os dados de cada barreira (x,y,tamanho)
+        }
+
+
+    }
+}
