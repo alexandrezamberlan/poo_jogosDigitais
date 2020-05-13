d069acda60c9261ae83ae9967d946741190451c6
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-05-13 14:06:31 -0300

    menu jogo da forca

--- 4-jogoDaForca/4-jogoDaForca/Program.cs
+++ 4-jogoDaForca/4-jogoDaForca/Program.cs
@@ -18,24 +18,53 @@ namespace _4_jogoDaForca
                 Console.WriteLine("4 - visualizar pontuação");
                 Console.WriteLine("5 - sair");
 
-
                 Console.Write("Opção: ");
                 opcao = Int32.Parse( Console.ReadLine() );
 
                 switch (opcao)
                 {
                     case 1:
-                        Console.WriteLine("Cadastro de palavras");
+                        Console.WriteLine("Gestão de palavras");
                         //informar nome da palavra e a categoria
-                        Apoio.carregarPalavras(listaPalavras);
-                        Apoio.mostrarPalavras(listaPalavras);
+                        Console.Write("1 - Carrega palavras de arquivo; 2 - Grava palavras em arquivo: ");
+                        string op = Console.ReadLine();
+                        if (op.Equals("1"))
+                        {
+                            Util.lerArquivoEmLista(listaPalavras,listaCategorias);
+                        } else if (op.Equals("2"))
+                        {
+                            Util.escreverEmArquivo();
+                            Console.WriteLine("Gravação OK das palavras no arquivo.... Próximo passo");
+                            Util.lerArquivoEmLista(listaPalavras,listaCategorias);
+                        } else
+                        {
+                            Console.WriteLine("Você não escolheu uma opção correta");
+                        }
                         
-                        Apoio.limparTela();
+                        Util.mostrarPalavras(listaPalavras);
+                        Util.limparTela();
                         break;
                     case 2:
                         Console.WriteLine("Escolher categoria");
                         //Selecionar a categoria
+                        if (listaCategorias.Count > 0)
+                        {
+                            Util.mostrarCategorias(listaCategorias);
 
+                            int indiceCategoria;
+                            do
+                            {
+                                Console.Write("\n\nDigite o número da categoria: ");
+                                indiceCategoria = Int32.Parse(Console.ReadLine());
+                            } while (indiceCategoria < 0 || indiceCategoria >= listaCategorias.Count);
+
+                            categoriaEmJogo = listaCategorias[indiceCategoria];
+                            Console.WriteLine("OK... Você selecionou a categoria: " + categoriaEmJogo);
+                        } else
+                        {
+                            Console.WriteLine("Primeiro é preciso carregar a lista de palavras");
+                        }
+                        Util.limparTela();
                         break;
                     default:
                         break;
@@ -43,8 +72,6 @@ namespace _4_jogoDaForca
 
             } while (opcao != 5);
         }
-
-
         static void Main(string[] args)
         {
             Console.WriteLine("Jogo da Forca");
@@ -55,6 +82,7 @@ namespace _4_jogoDaForca
 
         static List<Palavra> listaPalavras = new List<Palavra>();
         static List<Jogador> listaJogadores = new List<Jogador>();
-
+        static List<string> listaCategorias = new List<string>();
+        static string categoriaEmJogo;
     }
 }
---
+++ 4-jogoDaForca/4-jogoDaForca/Util.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic; //importação para manipular List
+using System.IO; //importação para manipular arquivos
+using System.Text; //importação para manipular o Encoding de um texto
+
+namespace _4_jogoDaForca
+{
+    public class Util
+    {
+        public static void escreverEmArquivo()
+        {
+            String expressao;
+            String categoria;
+
+            String nomeArquivo;
+            Console.Write("Digite o nome do arquivo que deseja gravar: ");
+            nomeArquivo = Console.ReadLine();
+
+
+            //se o arquivo existe, adicionar as novas expressões e suas categorias
+            //caso contrário, criá-lo
+            StreamWriter escritor;
+
+            if (!File.Exists(nomeArquivo))
+            {
+                escritor = new StreamWriter(nomeArquivo);
+            }
+            else
+            {
+                escritor = File.AppendText(nomeArquivo);
+            }
+
+            do
+            {
+                Console.Write("Digite uma Expressão ou a palavra sair para finalizar: ");
+                expressao = Console.ReadLine();
+                if (expressao.ToUpper().Equals("SAIR"))
+                {
+                    break;
+                }
+
+                Console.Write("Digite a Categoria da Expressão: ");
+                categoria = Console.ReadLine();
+
+                escritor.WriteLine(expressao + "@" + categoria);
+                escritor.Flush();
+
+            } while (true);
+
+            escritor.Close();
+        }
+
+
+        public static void lerArquivoEmLista(List<Palavra> listaPalavras, List<string> listaCategorias)
+        {
+            try
+            {
+                List<String> frasesLidas = new List<string>();
+                String nomeArquivo;
+                Console.Write("Digite o nome do arquivo que deseja ler: ");
+                nomeArquivo = Console.ReadLine();
+                StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8); //abre o arquivo para leitura
+
+                do
+                {
+                    frasesLidas.Add(leitor.ReadLine());
+
+                } while (!leitor.EndOfStream);
+
+                leitor.Close(); //fecha o objeto que representa o arquivo
+
+
+                String[] resposta;
+
+                for (int i = 0; i < frasesLidas.Count; i++)
+                {
+                    //Console.WriteLine(frasesLidas[i]);
+
+                    //nova zelândia@país
+                    resposta = frasesLidas[i].Split("@");
+                    //resposta = ["nova zelândia", "país"];
+
+                    listaPalavras.Add(new Palavra(resposta[0].ToUpper(), resposta[1].ToUpper()));
+
+                    if (!listaCategorias.Contains(resposta[1].ToUpper()))
+                    {
+                        listaCategorias.Add(resposta[1].ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao ler o arquivo: " + e.Message);
+                //leArquivoEmLista(listaPalavras);
+            }
+
+        }
+
+        static public void mostrarPalavras(List<Palavra> lista)
+        {
+            Console.WriteLine("Palavras cadastradas");
+            Console.WriteLine("___________________________________________");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine("Palavra: " + lista[i].texto +
+                    " - Categoria: " + lista[i].categoria);
+            }
+            Console.WriteLine("___________________________________________");
+        }
+
+        static public void mostrarCategorias(List<string> lista)
+        {
+            Console.WriteLine("Categorias cadastradas");
+            Console.WriteLine("___________________________________________");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(i + ": " + lista[i]);
+            }
+            Console.WriteLine("___________________________________________");
+        }
+
+        static public void limparTela()
+        {
+            ////declarando uma string com o comando somente para facilitar a visualização
+            //string executar = @"/C " + "clear";
+
+            ////Executa o comando no cmd do windows e aguarda a execução do mesmo para fechá-lo
+            //System.Diagnostics.Process.Start("CMD.exe",executar).WaitForExit();
+            Console.Write("Pressione ENTER para continuar ");
+            Console.ReadKey();
+            System.Console.Clear();
+
+        }
+    }
+}
