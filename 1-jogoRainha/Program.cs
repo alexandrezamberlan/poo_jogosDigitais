2e3a92b8ea043a5de5e236cd127b40ab1c45a9e8
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-09-25 16:05:13 -0300

    alguns espaços em branco.....

--- 1-jogoRainha/Program.cs
+++ 1-jogoRainha/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            //comentário.... para fundamentos de eng. sw
+            //comentário.... para fundamentos de eng. sw    
             int dimensao;
 
             Console.Write("Informe a dimensão da matriz: ");
