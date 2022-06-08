9d1a3bfa0d111c9cf5c23ae24417fb208184cebc
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-06-08 20:50:29 -0300

    threads

---
+++ 2022/1-ListaExercicios/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicios
+{
+//Crie um programa em C# no qual o usuário deverá informar o nome e o tipo de cinco animais de estimação. 
+//O programa deverá exibir na tela para o usuário quantos Cachorros, Gatos e Peixes foram informados.
+
+//Regras que deverão ser seguidas para a implementação do programa:
+
+//Os únicos tipos de animais válidos são Cachorro, Gato, Peixe.
+//Caso seja informado um tipo diferente o programa repetir até o usuário digitar um tipo válido.
+//É obrigatório criar uma classe para representar o Animal.
+//A classe deverá possuir dois dados privados (propriedades) para representar as características do animal.
+//A classe deverá possuir métodos de acesso (propriedade getter e setter) para permitir que o usuário armazene/leia os dados dos dois dados privados(propriedades).
+    class Animal
+    {
+        string nome;
+        string tipo;
+        public static string[] tiposValidos =
+        {
+            "GATO",
+            "CACHORRO",
+            "PEIXE",
+            "TARTARUGA"
+        };
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Tipo { get => tipo; }
+
+        public Animal(string nome, string tipo)
+        {
+            this.nome = nome;
+            this.tipo = tipo;
+        }
+    }
+}
---
+++ 2022/1-ListaExercicios/Personagem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicios
+{
+//Implemente um programa em C# no qual o usuário deverá informar o nome e o poder (0 a 10) de três personagens. 
+//O programa deverá informar o nome do personagem que possuir o maior poder.
+
+//Regras que deverão ser seguidas para a implementação do algoritmo:
+
+//É obrigatório o uso de classe para representar um Personagem e a mesma deverá possuir como propriedades (características) um nome e um poder.
+//A classe também deverá possuir um método chamado ExibirDados.Esse método deverá exibir o nome e o poder do personagem em questão.
+//Ao implementar a classe é obrigatório implementar dois construtores (Sobrecarga), um que não recebe parâmetro algum e outro que
+//irá receber o nome e o poder de um personagem
+    class Personagem
+    {
+        string nome;
+        int poder; //0 a 10
+
+        public Personagem()
+        {
+
+        }
+
+        public Personagem(string nome, int poder)
+        {
+            this.nome = nome;
+            this.poder = poder;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Poder { get => poder; set => poder = value; }
+
+        public void exibirDados()
+        {
+            Console.WriteLine(Nome + " - " + Poder);
+        }
+
+        //metodo de classe, ou seja, soh a classe Personagem pode invoca-lo ou chama-lo
+        public static Personagem maisPoderoso(List<Personagem> lista)
+        {
+            Personagem poderoso = new Personagem();
+            //assumir que o primeiro personagem é o mais poderoso
+            poderoso.Nome = lista[0].Nome;
+            poderoso.Poder = lista[0].Poder;
+
+            foreach (var i in lista)
+            {
+                if (i.Poder > poderoso.Poder)
+                {
+                    poderoso.Nome = i.Nome;
+                    poderoso.Poder = i.Poder;
+                }
+            }
+            return poderoso;
+        }
+    }
+}
